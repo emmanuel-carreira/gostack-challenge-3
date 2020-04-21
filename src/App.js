5732eb1be772e9aa454ae0328f9923dcfaa97139
Emmanuel Carreira Alves <emmanuel_carreira@hotmail.com>
2020-04-20 23:47:01 -0300

    feat: Add api requests to manipulate repositories

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./styles.css";
+import api from "./services/api";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories')
+      .then(response => {
+        setRepositories(response.data);
+      });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    let data = {}
+    api.post('repositories', data)
+      .then(response => {
+        setRepositories([...repositories, response.data]);
+      })
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    api.delete(`repositories/${id}`)
+      .then(() => {
+        const repos = repositories.filter(repo => repo.id != id);
+        setRepositories(repos);
+      });
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repository =>
+            <li key={repository.id}>
+              {repository.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+              <button onClick={() => handleRemoveRepository(repository.id)}>
+                Remover
+              </button>
+            </li>
+        )}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
